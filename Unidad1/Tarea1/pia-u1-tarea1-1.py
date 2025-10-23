29979ba88019ef13ca4db0fc05e0bf061b0323f2
duceda <daniuceda_86@hotmail.com>
2025-10-23 12:41:03 +0200

    Correcciones tarea1

--- Unidad1/Tarea1/pia-u1-tarea1-1.py
+++ Unidad1/Tarea1/pia-u1-tarea1-1.py
@@ -1,28 +1,11 @@
-my_list_1 = [4, -1, 2, 4, 3, -5, 0, 2]
+my_list = [4, -1, 2, 4, 3, -5, 0, 2]
 
-print("Input list 1: " + str(my_list_1))
+print("Input list: " + str(my_list))
 
-
-## Opcion 1 - Dividirlo en funciones independientes
-def delete_negative_numbers(numList):
+def transform_list(numList):
     positiveList = list(filter(lambda x : x >= 0, numList))
-    return positiveList
+    uniqueList = set(positiveList)  
+    finalList = sorted(uniqueList)
+    return finalList
 
-def delete_duplicated_numbers(numList):
-    uniqueList = set(numList)
-    return uniqueList
-    
-
-def sort_list_numbers(numList):
-    sortedList = sorted(numList)
-    return sortedList
-
-print("Output list 1: " + str(sort_list_numbers(delete_duplicated_numbers(delete_negative_numbers(my_list_1)))))
-
-
-## Opcion 2 - Hacerlo todo en una línea importando numpy
-import numpy
-
-my_list_2 = [4, -1, 2, 4, 3, -5, 0,  2]
-print("Input list 2: " + str(my_list_2))
-print("Output list 2:" + str(list(numpy.unique(list(filter(lambda x : x >= 0, my_list_2))))))
+print("Output list: " + str(transform_list(my_list)))
--- Unidad1/Tarea1/pia-u1-tarea1-2.py
+++ Unidad1/Tarea1/pia-u1-tarea1-2.py
@@ -4,23 +4,19 @@ words_list = ["hello", "world", "house", "car", "python"]
 
 print("Input list: " + str(words_list))
 
-words_list.sort()
-
-print("Sorted list: " + str(words_list))
-
-def content_file_format(contentFile):
-    no_punctuation = re.sub(r'[^\w\s]', '', contentFile)
-    return no_punctuation
-
-with open('textFile.txt', 'r', encoding='utf-8') as f:
-    content = f.read().lower()
-
-fileContentFormatted = content_file_format(content)
-print("File content Formatted: " + fileContentFormatted)
-
-wordsDictionary = {}
-
-for word in words_list:
-    wordsDictionary[word] = fileContentFormatted.count(word)
+def get_dictionary(wordsList, filePath):
+    wordsDictionary = {}
     
-print("Final dictionary: " + str(wordsDictionary))
+    wordsList.sort()
+    
+    with open(filePath, 'r', encoding='utf-8') as f:
+        content = f.read().lower()
+
+    file_content_formatted = re.sub(r'[^\w\s]', '', content)
+
+    for word in wordsList:
+        wordsDictionary[word] = file_content_formatted.count(word)
+
+    return wordsDictionary
+    
+print("Final dictionary: " + str(get_dictionary(words_list, 'textFile.txt')))
